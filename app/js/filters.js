9a18458afdba7c6d66d979cc371f923fa79d979b
Stephan Melzer <melzer.stephan@gmail.com>
2013-04-03 10:35:49 +0200

    added file size filter

--- app/js/filters.js
+++ app/js/filters.js
@@ -1,8 +1,19 @@
 'use strict';
 
 angular.module('centerdevice.filters', []).
-  filter('interpolate', ['version', function(version) {
-    return function(text) {
-      return String(text).replace(/\%VERSION\%/mg, version);
-    }
-  }]);
+filter('fileSize', function() {
+	// taken from http://stackoverflow.com/questions/10420352/converting-file-size-in-bytes-to-human-readable#
+	var fileSizeFilter = function(fileSizeInBytes) {
+		var i = -1;
+		var byteUnits = [' kB', ' MB', ' GB', ' TB', ' PB', ' EB', ' ZB', ' YB'];
+		do {
+			fileSizeInBytes = fileSizeInBytes / 1024;
+			i++;
+		} while (fileSizeInBytes > 1024);
+
+		// no locale formatted string supported
+		return Math.max(fileSizeInBytes, 0.1).toFixed(1) + byteUnits[i];
+	};
+
+	return fileSizeFilter;
+});
