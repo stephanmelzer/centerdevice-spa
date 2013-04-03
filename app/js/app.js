f35881e7e5c2a4be9a1c38d20921ffffd8b5898a
Stephan Melzer <melzer.stephan@gmail.com>
2013-04-03 09:09:52 +0200

    added 401 interceptor which redirects on HTTP code 401 to welcome page.

--- app/js/app.js
+++ app/js/app.js
@@ -22,3 +22,45 @@ myModule.config(function($routeProvider, $locationProvider) {
 		redirectTo: '/'
 	});
 });
+
+// define 401 interceptor
+// taken from: http://www.espeo.pl/2012/02/26/authentication-in-angularjs-application
+myModule.config(function($httpProvider) {
+	var interceptor = ['$rootScope', '$q', function(scope, $q) {
+
+		function success(response) {
+			return response;
+		}
+
+		function error(response) {
+			var status = response.status;
+
+			if (status == 401) {
+				var deferred = $q.defer();
+				scope.$broadcast('event:loginRequired');
+
+				return deferred.promise;
+			}
+
+			// otherwise
+			return $q.reject(response);
+		}
+
+		return function(promise) {
+			return promise.then(success, error);
+		}
+	}];
+
+	$httpProvider.responseInterceptors.push(interceptor);
+});
+
+myModule.run(['$rootScope', '$http', function(scope, $http) {
+
+	/**
+	 * On 'loginRequired' redirect to welcome page to login in again.
+	 */
+	scope.$on('event:loginRequired', function() {
+		var welcomeUrl = window.location.protocol + "//" + window.location.host + "/app/welcome.html";
+		window.location.href = welcomeUrl;
+	});
+}]);
