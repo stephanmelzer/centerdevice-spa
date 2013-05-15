887163a5a3cd2007da15c38856c6ba08bb823230
Stephan Melzer <melzer.stephan@gmail.com>
2013-05-15 15:36:00 +0200

    added gzip/deflate support to server.js

--- server.js
+++ server.js
@@ -1,16 +1,68 @@
-var static = require('node-static');
-var file = new static.Server('./dist');
-var port = process.env.PORT || 5000;
-require('http').createServer(function(request, response) {
-	file.serve(request, response, function(err, res) {
-		if (err) { // An error as occured
-			console.error("> Error serving " + request.url + " - " + err.message);
-			response.writeHead(err.status, err.headers);
+//
+// Simple static HTTP server that uses gzip compression (if client accepts gzip encoding)
+// by Richard Dancsi, www.wimagguc.com
+//
+
+var DOCUMENT_ROOT = './dist';
+var DIRECTORY_INDEX = '/index.html';
+
+var port = process.env.PORT || 8000;
+
+var zlib = require('zlib');
+var http = require('http');
+var path = require('path');
+var fs = require('fs');
+
+http.createServer(function(request, response) {
+
+	// Remove query strings from uri
+	if (request.url.indexOf('?')>-1) {
+		request.url = request.url.substr(0, request.url.indexOf('?'));
+	}
+
+	// Remove query strings from uri
+	if (request.url == '/') {
+		request.url = DIRECTORY_INDEX;
+	}
+	var filePath = DOCUMENT_ROOT + request.url;
+
+	var extname = path.extname(filePath);
+
+	var acceptEncoding = request.headers['accept-encoding'];
+	if (!acceptEncoding) {
+		acceptEncoding = '';
+	}
+
+	path.exists(filePath, function(exists) {
+
+		if (exists) {
+			fs.readFile(filePath, function(error, content) {
+				if (error) {
+					response.writeHead(500);
+					response.end();
+				}
+				else {
+					var raw = fs.createReadStream(filePath);
+
+					if (acceptEncoding.match(/\bdeflate\b/)) {
+						response.writeHead(200, { 'content-encoding': 'deflate' });
+						raw.pipe(zlib.createDeflate()).pipe(response);
+					} else if (acceptEncoding.match(/\bgzip\b/)) {
+						response.writeHead(200, { 'content-encoding': 'gzip' });
+						raw.pipe(zlib.createGzip()).pipe(response);
+					} else {
+						response.writeHead(200, {});
+						raw.pipe(response);
+					}
+				}
+			});
+		}
+		else {
+			response.writeHead(404);
 			response.end();
-		} else { // The file was served successfully
-			console.log("> " + request.url + " - " + res.message);
 		}
 	});
+
 }).listen(port);
 
-console.log("> node-static is listening on http://127.0.0.1:" + port);
+console.log('Serving files on http://localhost:' + port);
